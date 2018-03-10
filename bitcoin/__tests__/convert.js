13c14b50bf17afd63e962855e53867f68da0fa2d
Vidushan CHOORIYAKUMARAN <vidushan.chooriyakumaran@hotmail.fr>
2018-03-10 23:17:15 +0100

    test(bitcoin): add test for convert - 100% code coverage

--- bitcoin/__tests__/convert.js
+++ bitcoin/__tests__/convert.js
@@ -48,7 +48,9 @@ test('should convert a NaN to a Number', () => {
   //convert(NaN, 'BTC', 'mBTC', 'Number');
   expect(convert(NaN, 'BTC', 'BTC', 'Number')).toBe(NaN);
   expect(convert(NaN, 'BTC', 'mBTC', 'Number')).toBe(NaN);
+  expect(convert(NaN, 'BTC', 'BTC')).toBe(NaN);
 });
+
 
 test('should convert a NaN to a String', () => {
   //convert(NaN, 'BTC', 'BTC', 'String');
@@ -59,7 +61,7 @@ test('should convert a NaN to a String', () => {
 
 test('should not convert a NaN to a Big', () => {
   //convert(NaN, 'BTC', 'BTC', 'Big');
-  expect(() => {convert(NaN, 'BTC', 'BTC', 'Big')}).toThrowError('NaN');
+  expect(() => { convert(NaN, 'BTC', 'BTC', 'Big') }).toThrowError('NaN');
 });
 
 test('should handle rounding errors', () => {
@@ -74,23 +76,43 @@ test('should throw when untest is undefined', () => {
   //convert(new Big(2), 'BTC', 'x', 'Number');
   //convert(NaN, 'x', 'BTC', 'Number');
   //convert(NaN, 'BTC', 'x', 'Number');
-  expect(() => {convert(new Big(2), 'x', 'BTC', 'Number')}).toThrow(/is not a bitcoin unit/);
-  expect(() => {convert(new Big(2), 'BTC', 'x', 'Number')}).toThrow(/is not a bitcoin unit/);
-  expect(() => {convert(NaN, 'x', 'BTC', 'Number')}).toThrow(/is not a bitcoin unit/);
-  expect(() => {convert(NaN, 'BTC', 'x', 'Number')}).toThrow(/is not a bitcoin unit/);
+  expect(() => { convert(new Big(2), 'x', 'BTC', 'Number') }).toThrow(/is not a bitcoin unit/);
+  expect(() => { convert(new Big(2), 'BTC', 'x', 'Number') }).toThrow(/is not a bitcoin unit/);
+  expect(() => { convert(NaN, 'x', 'BTC', 'Number') }).toThrow(/is not a bitcoin unit/);
+  expect(() => { convert(NaN, 'BTC', 'x', 'Number') }).toThrow(/is not a bitcoin unit/);
 });
 
 test('should throw when representaion is undefined', () => {
   //convert(2, 'BTC', 'mBTC', 'x');
   //convert(NaN, 'BTC', 'mBTC', 'x');
-  expect(() => {convert(2, 'BTC', 'mBTC', 'x')}).toThrow(/is not a valid representation/);
-  expect(() => {convert(NaN, 'BTC', 'mBTC', 'x')}).toThrow(/is not a valid representation/);
+  expect(() => { convert(2, 'BTC', 'mBTC', 'x') }).toThrow(/is not a valid representation/);
+  expect(() => { convert(NaN, 'BTC', 'mBTC', 'x') }).toThrow(/is not a valid representation/);
 });
 
 test('should allow untest aliases', () => {
   //convert(4.6, 'Satoshi', 'sat');
   //convert(4.6, 'μBTC', 'btest');
   expect(convert(4.6, 'Satoshi', 'sat')).toBe(4.6);
-  expect(() => {convert(4.6, 'μBTC', 'btest')}).toThrow(/is not a bitcoin unit/);
+  expect(() => { convert(4.6, 'μBTC', 'btest') }).toThrow(/is not a bitcoin unit/);
 
+});
+
+test('should add a unit', () => {
+  convert.addUnit('vidu', 0.060496);
+  const units = convert.units();
+  expect(units[units.length - 1]).toBe('vidu');
+});
+
+test('should throw when unit has already a factor', () => {
+  expect(() => { convert.addUnit('BTC', 0.100717) }).toThrow(/already exists with a different conversion factor/);
+});
+
+test('should remove a unit', () => {
+  convert.removeUnit('vidu');
+  const units = convert.units();
+  expect(units[units.length - 1]).toBe('sat');
+});
+
+test('should throw when a predefined unit is removed', () => {
+  expect(() => { convert.removeUnit('BTC') }).toThrow(/is predefined and cannot be removed/)
 });
